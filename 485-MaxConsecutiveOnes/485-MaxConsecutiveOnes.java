91ce2badbf7286dd5bc0a6620744424d9f51df9b
Rahul Ranjan <166069140+SairentoKira707@users.noreply.github.com>
2026-04-20 15:55:22 +0530

    Create 485-MaxConsecutiveOnes

---
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
@@ -0,0 +1,17 @@
+// Last updated: 20/04/2026, 15:55:21
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        for(int num:nums){
+            if(num==1){
+                currentCount++;
+                maxCount=Math.max(maxCount,       currentCount);
+            }
+            else{
+                currentCount =0;
+            }
+        }
+        return maxCount;
+    }
+}
