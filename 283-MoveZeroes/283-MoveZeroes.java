dbfe4bf0ef2be6151ea7cc1ac3a00a17896d3f61
Rahul Ranjan <166069140+SairentoKira707@users.noreply.github.com>
2026-04-20 15:55:26 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,21 @@
+// Last updated: 20/04/2026, 15:55:25
+class Solution {
+    static void moveZeroes(int[] arr) {
+        int count=0;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[count];
+                arr[count]=temp;
+                count++;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,0,4,3,0,5,0};
+        moveZeroes(arr);
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
