ddf86771a78914c40beabfc8e1981f8c43db0ff2
Rahul Ranjan <166069140+SairentoKira707@users.noreply.github.com>
2026-04-20 15:55:06 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,11 @@
+// Last updated: 20/04/2026, 15:55:06
+/**
+ * @return {Function}
+ */
+
+    const createHelloWorld = () => () => "Hello World";
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
